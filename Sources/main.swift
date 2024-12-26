5b90d57e6cb375f952eb5a8d23d316a879c91a5f
aliAkkayaJ <aliakkayaa0@gmail.com>
2024-12-26 17:57:52 +0300

    add: strings

--- Sources/main.swift
+++ Sources/main.swift
@@ -48,6 +48,57 @@ func calculateTotalPrice(packageType: String = "free") -> Double {
 // print("Total price for free package: \(calculateTotalPrice(packageType: "pro"))")
 
 // 3. Strings and Characters
+let quote = """
+
+        The quick brown fox jumps over \t the lazy dog.
+        The quick brown fox jumps over \n the lazy dog.
+        The quick brown fox jumps over the lazy dog.
+        The quick brown fox jumps over the lazy dog.
+        The quick brown fox jumps over the lazy dog.
+
+    """
+let sparklingHeart = "\u{1F496}"  // 💖, Unicode scalar U+1F496
+
+print(quote)
+
+var name = ""
+let surname = String()
+name += "Ali"
+print(name)
+
+let letterArray: [Character] = ["A", "l", "i"]
+for letter in letterArray {
+    if letter == "i" {
+        print("i found")
+    } else {
+        print("i not found")
+    }
+}
+name = String(letterArray)  // The arrays converted the string
+
+// append method
+name.append("Akkaya")
+print(name)
+
+// String Interpolation
+let age: Double = 25
+let message = "I am \(Int(age)) years old."
+print(message)
+
+// Counting Characters
+print("The length of the name is \(name.count)")
+// Accessing and Modifying a String
+let greeting = "Hello, world!"
+greeting[greeting.startIndex]  // H
+greeting[greeting.index(before: greeting.endIndex)]  // ,
+greeting[greeting.index(after: greeting.startIndex)]  // e
+// let index = greeting.index(greeting.startIndex, offsetBy: 3)
+// greeting[index]  // l
+
+//substring
+let index = greeting.firstIndex(of: ",") ?? greeting.endIndex
+let beginning = greeting[..<index]
+let newString = String(beginning)
 // 4. Collection Types
 // 5. Control Flow
 // 6. Functions
